13d636e81555c5ccc7f8e7c41c102d7d2fccb556
chrisnathan20 <christopher.nathanael@mail.utoronto.ca>
2024-01-17 02:24:46 -0500

    implemented functional ToDoApp in React

---
+++ ToDoList/components/AddTask.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+const AddTask = ({ onAddTask }) => {
+  const [inputTitle, setInputTitle] = useState('');
+
+  const handleAddTask = () => {
+    if (inputTitle.trim() !== '') {
+      onAddTask(inputTitle.trim());
+      setInputTitle('');
+    }
+  };
+
+  return (
+    <View style={styles.addTodoForm}>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter new To Do"
+        value={inputTitle}
+        onChangeText={(text) => setInputTitle(text)}
+        keyboardType="numeric"
+        returnKeyType="done"
+      />
+      <Button title="Add To Do Item" onPress={handleAddTask} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    addTodoForm: {
+      margin: 10,
+    },
+    input: {
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      marginBottom: 10,
+      paddingHorizontal: 10,
+    },
+});
+
+export default AddTask;
---
+++ ToDoList/components/ToDoList.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { v4 as uuidv4 } from 'uuid';
+
+import AddTask from './AddTask';
+
+const Todo = ({ initialValues }) => {
+  const [toDos, setToDos] = useState(initialValues.map((value) => ({ id: uuidv4(), title: value })));
+
+  const addToDo = (newTitle) => {
+    const newToDo = { id: uuidv4(), title: newTitle };
+    setToDos((prevToDos) => [...prevToDos, newToDo]);
+  };
+
+  const removeToDo = (id) => {
+    setToDos(prevTodos => prevTodos.filter(toDo => toDo.id !== id));
+  };
+
+  return (
+    <View style={styles.todoListContainer}>
+      {toDos.map((toDo) => (
+        <View key={toDo.id} style={styles.todoItem}>
+          <Text>{toDo.title} </Text>
+          <View>
+            <Button title="Remove" onPress={() => removeToDo(toDo.id)} />
+          </View>
+        </View>
+      ))}
+      <AddTask onAddTask={addToDo} />
+    </View>
+  );
+};
+
+Todo.defaultProps = {
+  initialValues: [],
+};
+
+const styles = StyleSheet.create({
+  todoListContainer: {
+    margin: 10,
+  },
+  todoItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    marginVertical: 5,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+  }
+});
+
+export default Todo;
